0ed921db522b255975268c19948faaa27751ccda
Jeffery Durand <durand.jeffery@gmail.com>
2021-02-08 01:13:58 +0100

    reorganized folder and added sliders

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # ATIAM_webaudio_devoir
 Devoir de web audio pour ATIAM 2020-2021
 
+Javascript:
+Waveform Generation inspiration @ https://tonejs.github.io/examples/signal
+Graph inspiration inpiration @ https://glitch.com/edit/#!/dfpi-audio-waveform?path=sketch.js
+
+CSS:
+CSS Template @ https://templated.co/caminar
+
+Images:
 Favicon generated with geogebra and formated @ https://favicomatic.com/
+Background image @ https://www.imaginefactory.com/wp-content/uploads/2017/04/SPACES_BG-2.jpg
+
--- files/modulator.js
+++ files/modulator.js
@@ -1,3 +1,7 @@
+//////////////////////////
+// Create the waveform
+//////////////////////////
+
 // panners for seperating our 2 oscillators
 const channelRight = new Tone.PanVol().toDestination();
 const channelLeft = new Tone.PanVol().toDestination();
@@ -13,8 +17,8 @@ var leftOsc = new Tone.Oscillator({
 }).connect(channelLeft);
 
 // the frequency signal
-const frequency = new Tone.Signal(0.5);
-const scale = new Tone.ScaleExp(440, 880);
+const frequency = new Tone.Signal(0.1);
+const scale = new Tone.ScaleExp(20, 8000);
 const mult = new Tone.Multiply(1);
 
 // chain the components together
@@ -22,27 +26,40 @@ frequency.chain(scale, mult);
 scale.connect(leftOsc.frequency);
 mult.connect(rightOsc.frequency);
 
-// Create a new canvas to the browser size
-function setup () {
-	
-	var canvas = createCanvas(windowWidth, windowHeight);
-	canvas.parent('oscilloscope')
-	// Clear with black on setup
-	background(0);
 
-	// Create an analyser node that makes a waveform
+//////////////////////////
+// Draw the waveform
+//////////////////////////
+
+// define drawing of the canvas
+function canvasMaker() {
+	const width = document.getElementById("oscilloscope").offsetWidth
+	var canvas = createCanvas(width, width / 3);
+	canvas.parent('oscilloscope')
+}
+
+// setup for p5.js library
+function setup () {
+
+	// draw the canvas
+	canvasMaker()
+
+	// set the grey background
+	background('#1e1f24');
+
+	// create an analyser node that makes a waveform
 	analyser = new Tone.Analyser('waveform', 1024);
 
 	// Connect with analyser as well so we can detect waveform
 	Tone.Master.connect(analyser);
 
-	// define frame so the oscilloscope is understandable
+	// lower frame rate so the oscilloscope is viewable
 	frameRate(30);
 }
 
 // On window resize, update the canvas size
 function windowResized () {
-	resizeCanvas(windowWidth, windowHeight);
+	canvasMaker()
 }
 
 // Render loop that draws shapes with p5
@@ -51,27 +68,27 @@ function draw() {
 	const dim = Math.min(width, height);
 	const values = analyser.getValue();
 	
-	//if (abs(values[0]) < 0.1 && values[0] <= values[1]) {
-		// White background
-		background('#fff');
+	background('#1e1f24');
 
-		strokeWeight(dim * 0.0025);
-		stroke(0);
-		noFill();
-		beginShape();
-		for (let i = 0; i < values.length; i++) {
-		  const amplitude = values[i];
-		  const x = map(i, 0, values.length - 1, 0, width);
-		  const y = height / 2 + amplitude * height;
-		  // Place vertex
-		  vertex(x, y);
-		}
-		endShape();
-	//}
+	strokeWeight(dim * 0.0025);
+	stroke('#f4f4f4');
+	noFill();
+	beginShape();
+	for (let i = 0; i < values.length; i++) {
+	  const amplitude = values[i];
+	  const x = map(i, 0, values.length - 1, 0, width);
+	  const y = height / 2 + amplitude * height;
+	  vertex(x, y);
+	}
+	endShape();
 }
 
 
-// Interactive
+//////////////////////////
+// React to the controls
+//////////////////////////
+
+// play button
 document.getElementById("player").addEventListener("click", updatePlayer);
 function updatePlayer() {
     if (player.textContent === "Play") {
@@ -85,23 +102,23 @@ function updatePlayer() {
     }
 }
 
-
-// ramp the frequency with the slider
+// volume control
 document.getElementById("volume").addEventListener("input", e => {
-	Tone.Master.volume.value = parseFloat(e.target.value);
+	newVolume = 20 * parseFloat(e.target.value) - 30
+	Tone.Master.volume.value = newVolume;
 });
 
-
-// ramp the frequency with the slider
+// pitch control
 document.getElementById("pitch-all").addEventListener("input", e => {
 	frequency.rampTo(parseFloat(e.target.value), 0.1);
 });
-// ramp the frequency with the slider
+
+// split the pitches
 document.getElementById("pitch").addEventListener("input", e => {
 	mult.rampTo(parseFloat(e.target.value), 0.1);
 });
 
-
+// split the panning
 document.getElementById("space").addEventListener("input", e => {
 	channelLeft.pan.value = parseFloat(-e.target.value);
 	channelRight.pan.value = parseFloat(e.target.value);
